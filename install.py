86243152e05d88b6bfb76166ef6b3266052f9004
Steven Sheldon <steven@sasheldon.com>
2012-12-11 11:06:34 -0600

    Began porting install script to Python.

---
+++ install.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+links = {
+	'screenrc':   '.screenrc',
+	'ackrc':      '.ackrc',
+	'toprc':      '.toprc',
+	'dir_colors': '.dir_colors',
+	'lessfilter': '.lessfilter',
+
+	'vim':      '.vim',
+	'vimrc':    '.vimrc',
+	'_vimrc':   '_vimrc',
+	'gvimrc':   '.gvimrc',
+
+	'emacsrc':  '.emacs',
+	'emacs':    '.emacsdir',
+
+	'commonsh': '.commonsh',
+
+	'inputrc':  '.inputrc',
+
+	'bash':         '.bash',
+	'bashrc':       '.bashrc',
+	'bash_profile': '.bash_profile',
+
+	'zsh':      '.zsh',
+	'zshrc':    '.zshrc',
+
+	'ksh':      '.ksh',
+	'kshrc':    '.kshrc',
+	'mkshrc':   '.mkshrc',
+
+	'shinit':   '.shinit',
+
+	'Xdefaults':  '.Xdefaults',
+	'Xresources': '.Xresources',
+
+	'uncrustify.cfg': '.uncrustify.cfg',
+	'indent.pro':     '.indent.pro',
+
+	'xmobarrc':  '.xmobarrc',
+	'xmonad.hs': '.xmonad/xmonad.hs',
+
+	'Wombat.xccolortheme':  'Library/Application Support/Xcode/Color Themes/Wombat.xccolortheme',
+#	'Wombat.dvtcolortheme': 'Library/Developer/Xcode/UserData/FontAndColorThemes/Wombat.dvtcolortheme',
+
+	'gitconfig': '.gitconfig',
+	'gitignore': '.gitignore',
+
+	'tigrc':     '.tigrc',
+
+	'caffeinate': 'bin/caffeinate',
+	'lock':       'bin/lock',
+
+	'git-info':            'bin/git-info',
+	'git-untrack-ignored': 'bin/git-untracked-ignored',
+
+	'gdbinit': '.gdbinit',
+}
